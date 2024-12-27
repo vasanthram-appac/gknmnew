7f65013c738d819b7c9fba2c5efb71c16ff5f214
JayaSuriyaAppac <surya.appac@gmail.com>
2024-12-27 17:28:00 +0530

    specialist page change

--- frontend/src/Web/components/header/main.jsx
+++ frontend/src/Web/components/header/main.jsx
@@ -1,12 +1,11 @@
 import Header from "./header";
 import Footer from "./footer";
 import Home from "../Pages/home";
-<<<<<<< HEAD
-import Specialities from "../Pages/Specialities";
+import Specialities from "../Pages/specialities";
 import "../webstyle.css";
-=======
 
->>>>>>> c534659f177d2f318ab2e81ae80d2d52fbc2653a
+
+
 
 
 import { Routes, Route } from "react-router-dom";
