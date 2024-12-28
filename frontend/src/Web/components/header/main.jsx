02da4a6cd55309d853a255a81b87f10a8dc8020a
Ramprasath Appac <ramprasathappac@gmail.com>
2024-12-28 09:44:34 +0530

    sat

--- frontend/src/Web/components/header/main.jsx
+++ frontend/src/Web/components/header/main.jsx
@@ -1,18 +1,12 @@
 import Header from "./header";
 import Footer from "./footer";
 import Home from "../Pages/home";
-<<<<<<< HEAD
 
 import Specialities from "../Pages/Specialities";
 import "../webstyle.css";
 
-=======
-import Specialities from "../Pages/specialities";
-import "../webstyle.css";
 
 
-
->>>>>>> fe674c92e92f7816edebc7e582a5fbe093f7e967
 
 
 import { Routes, Route } from "react-router-dom";
